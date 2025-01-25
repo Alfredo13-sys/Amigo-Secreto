ab7a3aa3de2486131621ca8f513f12008a4d6b95
Alfredo Islas <alfredoislassanchez8@gmail.com>
2025-01-25 12:10:36 -0600

    validacion de nombre

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Declaracion de variables
+let amigos = []
+let contenido
+
+//revisa que el dato sea un nombre y lo agrega a la lista de amigos
+function agregarAmigo(){
+    contenido = document.getElementById('amigo').value.trim()
+    if(contenido == ""){
+        alert("Por favor, inserte un nombre")
+    }
+    else{
+        amigos.push(contenido)
+    }
+    console.log(amigos)
+    limpiar()
+}
+
+//Limpia la barra de entrada de texto
+function limpiar () {
+    document.getElementById('amigo').value=""
+}
+
